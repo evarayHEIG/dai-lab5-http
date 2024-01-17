66a155d30afd02ada6a9abfdc62918f204ab6c00
evarayHEIG <eva.ray@heig-vd.ch>
2024-01-17 20:01:03 +0100

    Change time timeout in setInterval

--- docker/nginx/website/js/api.js
+++ docker/nginx/website/js/api.js
@@ -8,22 +8,20 @@ function fetchSongs() {
         // we convert them into json
         response.json().then(data => {
             console.log(data)
-            // iterate on each element of data
             let song = data[index]
             index = (index + 1) % (Object.values(data).length + 1);
             // create new div
             let div = document.createElement('div')
             // we add data to div
-            div.innerHTML = `<p>${song.title} - ${song.singer}</p>`
+            div.innerHTML = `<p>${song.singer} - ${song.title}</p>`
             // we retrieve the div in which we want to inject the data
             const divToInject = document.getElementsByClassName("api-inject")[0]
             divToInject.innerHTML = ""
             // we add the new div to the dom
             divToInject.appendChild(div)
-
         })
     })
 }
 
 // Call fetchSongs every 5 seconds
-setInterval(fetchSongs, 5000)
+setInterval(fetchSongs, 4000)
